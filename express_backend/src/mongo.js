bdbfe45fddaabd03d39d2ef4053fbf8b4ecb4f38
Priyanka Pandey <32104339+prnk04@users.noreply.github.com>
2025-11-16 22:07:38 +0530

    code fix
    code fix

--- express_backend/src/mongo.js
+++ express_backend/src/mongo.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 async function connectMongo() {
   try {
     // const uri = process.env.MONGODB_URI;
-    const uri = process.env.MONGO_ATLAS;
+    const uri = process.env.MONGODB_URI;
     if (!uri) throw new Error("Mongo uri is missing");
     await mongoose.connect(uri, { autoIndex: true });
+
     console.log("Connected to mongo db");
   } catch (error) {
     console.log("Error while connecting to mongo db: ", error);
